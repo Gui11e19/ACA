28997af1efbd45b003b06d755b827e54354cb0b9
aacm13 <00354817@uca.edu.sv>
2022-07-05 20:01:21 -0600

    User models documentation

--- models/User.js
+++ models/User.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+//Schema for user
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-		unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
-UserSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-        if (err) {
-        return next(err);
-        }
-            user.password = hash;
-        
-        next();
-    });
-    });
-module.exports = mongoose.model('user', UserSchema);
+UserSchema.pre("save", function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+
+    next();
+  });
+});
+module.exports = mongoose.model("user", UserSchema);
