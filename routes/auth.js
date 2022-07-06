5db369ff9f6e2d4b695c2aae0df629f0cc0c7889
aacm13 <00354817@uca.edu.sv>
2022-07-05 20:01:59 -0600

    auth route documentation

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const passport = require('passport');
-const User = require('../models/User.js');
-const AuthController = require('../controllers/authController.js')
+const passport = require("passport");
+const User = require("../models/User.js");
+const AuthController = require("../controllers/authController.js");
+//declaring auth routs
+router.post("/register", AuthController.register);
 
-router.post('/register', AuthController.register);
+router.post("/login", AuthController.login);
 
-router.post('/login', AuthController.login);
-
-router.post('/logout', AuthController.logout);
-router.get('/login', (req, res, next) => {
-	res.render('login',{title:'login'});
-}
-);
-
-router.get('/register', (req, res, next) => {
-	res.render('register',{title:'register'});
-}
-);
-
-router.get('/logout', function(req, res, next) {
-
-	res.render('logout', { title: 'Express - Test' });
+router.post("/logout", AuthController.logout);
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "login" });
 });
 
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "register" });
+});
+
+router.get("/logout", function (req, res, next) {
+  res.render("logout", { title: "Express - Test" });
+});
 
 module.exports = router;
