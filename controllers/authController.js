b23b436d5c314edac917d29d56568f1b81ac55a2
aacm13 <00354817@uca.edu.sv>
2022-07-05 19:47:29 -0600

    documenting AuthController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,51 +1,61 @@
-const passport = require('passport');
-const User = require('../models/User.js');
+const passport = require("passport");
+const User = require("../models/User.js");
 const AuthController = {};
+//register function
+AuthController.register = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  //checks if username already exixsts
+  User.findOne({ username: req.body.username }, function (err, user) {
+    if (err) {
+      return res.redirect("/");
+    }
+    if (user) {
+      return res.redirect("/auth/login");
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+      });
+      //saves the new user to the db
+      newUser
+        .save()
+        .then((user) => {
+          return res.send(user);
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.send("Error");
+        });
+    }
+  });
+};
+//login function
+AuthController.login = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
 
-AuthController.register = function(req, res, next) {
-	if (req.isAuthenticated()) {
-		return res.redirect('/')
-	}
-	User.findOne({ username: req.body.username }, function(err, user) {
-		if (err) {
-			return res.redirect('/');
-		}
-		if (user) {
-			return res.redirect('/auth/login');
-		} else {
-			const newUser = new User({
-				username: req.body.username,
-				password: req.body.password
-			})
-			newUser.save().then(user => {
-				
-				return	res.send(user);
-			}).catch(err => {
-				console.error(err);
-				return res.send('Error');
-			});
-		}
-	});
-}
-
-AuthController.login = function(req, res, next) {
-	if (req.isAuthenticated()) {
-		return res.redirect('/');
-	}
-
-	passport.authenticate('local', function(err, user, info) {
-		if (err) { return next(err); }
-		if (!user) { return res.redirect('/auth/login'); }
-		req.logIn(user, function(err) {
-			if (err) { return next(err); }
-			return res.redirect('/welcome');
-		});
-	})(req, res, next);
-}
-
-AuthController.logout = function(req, res, next) {
-	req.logout();
-	res.redirect('/');
-
-}
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    //if not logged in, should redirect to login page
+    if (!user) {
+      return res.redirect("/auth/login");
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/welcome");
+    });
+  })(req, res, next);
+};
+//logout function
+AuthController.logout = function (req, res, next) {
+  req.logout();
+  res.redirect("/");
+};
 module.exports = AuthController;
